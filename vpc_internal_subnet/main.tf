64f8cb8c8711c43acb915a1c624deafc79da3c94
Stephen Robinson <stephen@stephen-robinson.co.uk>
2018-06-25 14:57:57 +0100

    Add VPC internal subnet module

---
+++ vpc_internal_subnet/main.tf
@@ -0,0 +1,44 @@
+variable "name" {
+  description = "Name to give the route table"
+}
+
+variable "environment" {
+  description = "Environment tag, e.g. sandbox, dev, staging, prod"
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC to setup DHCP for"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR of the subnet to create"
+}
+
+variable "subnet_az" {
+  description = "Availability zone to create the subnet in"
+}
+
+variable "route_table_id" {
+  description = "The ID route table to associate the subnet table with"
+}
+
+resource "aws_subnet" "internal" {
+  vpc_id            = "${var.vpc_id}"
+  cidr_block        = "${var.subnet_cidr}"
+  availability_zone = "${var.subnet_az}"
+
+  tags {
+    Name        = "${var.name}"
+    Environment = "${var.environment}"
+    terraform   = "true"
+  }
+}
+
+resource "aws_route_table_association" "internal" {
+  subnet_id      = "${aws_subnet.internal.id}"
+  route_table_id = "${var.route_table_id}"
+}
+
+output "subnet_id" {
+  value = "${aws_subnet.internal.id}"
+}
